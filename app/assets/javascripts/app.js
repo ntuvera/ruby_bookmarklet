5830b6501404335d183e609b03c243ef81e3d25d
ntuvera <ntuvera@gmail.com>
2014-09-30 12:28:15 -0400

    mock ajax function, controller to accept json

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -1,3 +1,17 @@
-function(){
+function alertTest(){
   alert('this is only a test');
+}
+
+function addVideo(){
+  $.ajax({
+    url: 'localhost:3000/videos',
+    type: 'POST',
+    data: {video: {title: 'Shadow of Mordor', url: 'https://www.youtube.com/watch?v=GDfAIGxOSTw'} },
+    success: function(data){
+      console.log('data sent');
+    },
+    error: function(){
+      console.log('post denied, sucka');
+    }
+  })
 }
--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -5,8 +5,22 @@ class VideosController < ApplicationController
   end
 
   def create
+    @video = Video.new(video_params[:video])
+
+    respond_to do |f|
+      if @video.save
+        f.html { redirect_to @video }
+        f.json { render json: @video, status: :created, location: @video }
+      else
+        f.html { render action: 'new' }
+        f.json { render jsoon: @video.errors, status: :unprocessable_entity }
+      end
+    end
   end
 
-  def bookmarklet
+  private
+
+  def video_params
+    params.require(:video).permit(:title, :url)
   end
 end
